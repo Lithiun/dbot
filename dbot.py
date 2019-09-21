7c2709f106ad2c53d57a64eee2a232da4d21e9ff
KLZ-0 <adrian@kalazi.com>
2019-09-22 00:15:53 +0200

    !commands and invalid command handling

--- dbot.py
+++ dbot.py
@@ -1,17 +1,23 @@
 import datetime
 import discord
-from discord.ext import commands
+from discord.ext import commands as cmds
 
 import config
 config = config.Config()
 
-bot = commands.Bot(command_prefix = config.command_prefix)
+bot = cmds.Bot(command_prefix=config.command_prefix)
+
+def command_list():
+    with open("commands.md", "r") as f:
+        txt = f.read()
+    return txt
 
 @bot.event
 async def on_ready():
     """If DBOT is ready"""
     print("Ready")
 
+# Commands
 @bot.command()
 async def test(ctx):
     await ctx.send(f"test successful!")
@@ -20,5 +26,14 @@ async def test(ctx):
 async def dt(ctx):
     dt = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
     await ctx.send(f"Current datetime is {dt}")
+
+@bot.command()
+async def commands(ctx):
+        await ctx.send(command_list())
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, cmds.errors.CommandNotFound):
+        await ctx.send("Invalid command, try one of these:\n" + command_list())
 
 bot.run(config.key)
