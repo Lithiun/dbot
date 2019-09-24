aaa5b61273ce7477f4a3e37d3f44a5d90b146e5f
KLZ-0 <adrian@kalazi.com>
2019-09-24 12:07:09 +0200

    Fix encoding of commands.md

--- cogs/base.py
+++ cogs/base.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 def command_list():
-    with open("commands.md", "r") as f:
+    with open("commands.md", "r", encoding="utf-8") as f:
         txt = f.read()
     return txt
 
